581f894791f6336e7a719f9ad2173ebd4c7dda5d
ayan shukla <ayanshukla4@gmail.com>
2021-06-05 21:39:00 +0530

    updated app.js

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ app.use(cors());
 
 initializeConnectionDB();
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+// }
 
-app.get('*', (req, res) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+// 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 app.get('/', (req, res) => {
   res.json({success: true, message: 'Prerogative ecommerce app server'});
